7e7bbed1e8efe4b5474c211cb7bb6e92cdc1d4ac
guidogimeno <gimenoguido@gmail.com>
2018-09-26 09:30:05 -0300

    Agrego Caratula

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/*
+ * UTN - FRBA - Sintaxis y Semántica de los Lenguajes
+ * TP 03 - 2018
+ * Título: Scanner en Flex
+ * GRUPO NRO 1:            
+ * +-------------------------+-----------------------------------+----------+
+ * |    Apellido y Nombre    |               Email               |  Legajo  |
+ * +-------------------------+-----------------------------------+----------+
+ * | Gimeno Guido            | gimenoguido@gmail.com             | 156381-6 |
+ * | Mateika Hernán          | her.mateika@gmail.com             | 162746-6 |
+ * | Vicente Sebastián Ariel | vicente.sebastian.ariel@gmail.com | 155725-7 |
+ * | Vinocur Adrian          | adrian.vinocur@gmail.com          | 163002-7 |
+ * +-------------------------+-----------------------------------+----------+ 
+*/
+
 char *token_names[] = {"Fin de Archivo", "Identificador", "Constante", "Programa", "Variables", 
 	    	       "Definir", "Código", "Leer", "Asignación", "Escribir", "Fin"};
 int main() {
