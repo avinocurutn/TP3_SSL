381252f8f35ea7de8c29ca7b8f47e3cea193d124
guidogimeno <gimenoguido@gmail.com>
2018-10-08 23:08:04 -0300

    Correcciones

--- main.c
+++ main.c
@@ -13,21 +13,20 @@
  * +-------------------------+-----------------------------------+----------+ 
 */
 
+#include <stdio.h>
+#include "tokens.h"
+
+extern int yylex();
+extern char* yytext;
+
 char *token_names[] = {"Fin de Archivo", "Identificador", "Constante", "Programa", "Variables", 
 	    	       "Definir", "Código", "Leer", "Asignación", "Escribir", "Fin"};
 int main() {
 	enum token t;
 	while ((t = yylex()))
 		switch (t) {
-			case Programa: printf("Token: %s\n", token_names[t]); break;
-			case Variables: printf("Token: %s\n", token_names[t]); break;
-			case Definir: printf("Token: %s\n", token_names[t]); break;
-			case Codigo: printf("Token: %s\n", token_names[t]); break;
-			case Leer: printf("Token: %s\n", token_names[t]); break;
-			case Asignacion: printf("Token: %s\n", token_names[t]); break;
-			case Escribir: printf("Token: %s\n", token_names[t]); break;
-			case Fin: printf("Token: %s\n", token_names[t]); break;
 			case Identificador: case Constante: printf("Token: %s\tlexema: %s\n", token_names[t], yytext); break;
+			default: printf("Token: %s\n", token_names[t]); break;
 		}
 	printf("%s\n", token_names[t]);
 	return 0;
